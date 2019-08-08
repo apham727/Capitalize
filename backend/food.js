24c5f1c27bce66a9c569c14ddab119de6800a344
Eileen Chen <ezch09@gmail.com>
2019-08-08 16:30:30 -0400

    destinations.json

--- backend/food.js
+++ backend/food.js
@@ -1,1 +1,25 @@
 
+'use strict';
+
+const yelp = require('yelp-fusion');
+
+// Place holder for Yelp Fusion's API Key. Grab them
+// from https://www.yelp.com/developers/v3/manage_app
+const apiKey = '4mDizWWU3VWbTEPt_wlGaCvYkzQkvDhBrGiSTxKIXyyNnyOMiLqeyDBSZ357CcFsGuolnl34sF4Byjy64KFwtFHkvUcrw7suHDXFeIN6_iBmO4q4FHV1Bw_m7ltMXXYx';
+
+const searchRequest = {
+  term:'Four Barrel Coffee',
+  location: loc
+};
+
+const client = yelp.client(apiKey);
+
+client.search(searchRequest).then(response => {
+  const firstResult = response.jsonBody.businesses[0];
+  const prettyJson = JSON.stringify(firstResult, null, 4);
+  console.log(prettyJson);
+}).catch(e => {
+  console.log(e);
+});
+
+
