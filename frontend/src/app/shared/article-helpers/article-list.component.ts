e07e549402c02af551077dca04ea4a09e4e37cad
Andrew Pham <apham727@gmail.com>
2019-08-08 20:55:13 -0400

    preview list pulls from json

--- frontend/src/app/shared/article-helpers/article-list.component.ts
+++ frontend/src/app/shared/article-helpers/article-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 import { Article, ArticleListConfig, ArticlesService } from '../../core';
 @Component({
@@ -8,7 +10,8 @@ import { Article, ArticleListConfig, ArticlesService } from '../../core';
 })
 export class ArticleListComponent {
   constructor (
-    private articlesService: ArticlesService
+    private articlesService: ArticlesService,
+    private http: HttpClient
   ) {}
 
   @Input() limit: number;
@@ -27,28 +30,42 @@ export class ArticleListComponent {
   currentPage = 1;
   totalPages: Array<number> = [1];
 
+  // STORES THE PACKAGES LIST 
+  packages;
+
+
   setPageTo(pageNumber) {
     this.currentPage = pageNumber;
     this.runQuery();
   }
 
   runQuery() {
-    this.loading = true;
-    this.results = [];
+    // this.loading = true;
+    // this.results = [];
 
-    // Create limit and offset filter (if necessary)
-    if (this.limit) {
-      this.query.filters.limit = this.limit;
-      this.query.filters.offset =  (this.limit * (this.currentPage - 1));
-    }
+    // // Create limit and offset filter (if necessary)
+    // if (this.limit) {
+    //   this.query.filters.limit = this.limit;
+    //   this.query.filters.offset =  (this.limit * (this.currentPage - 1));
+    // }
 
-    this.articlesService.query(this.query)
-    .subscribe(data => {
-      this.loading = false;
-      this.results = data.articles;
+    // this.articlesService.query(this.query)
+    // .subscribe(data => {
+    //   this.loading = false;
+    //   this.results = data.articles;
 
-      // Used from http://www.jstips.co/en/create-range-0...n-easily-using-one-line/
-      this.totalPages = Array.from(new Array(Math.ceil(data.articlesCount / this.limit)), (val, index) => index + 1);
-    });
+    //   // Used from http://www.jstips.co/en/create-range-0...n-easily-using-one-line/
+    //   this.totalPages = Array.from(new Array(Math.ceil(data.articlesCount / this.limit)), (val, index) => index + 1);
+    // });
+
+    // LOGIC TO GENERATE ARTICLE LIST
+    this.http.get("assets/SampleResponse.json")
+    .subscribe(
+      result => {
+        this.packages = result
+        
+        
+      }
+    );
   }
 }
