eb58e6512008cd54943edf967c2626bd6eebd81b
Aalap Patel <aalappatel07@gmail.com>
2019-08-09 01:48:23 -0400

    functional expense tracker

--- frontend/src/app/expense-tracker/expense-tracker.component.ts
+++ frontend/src/app/expense-tracker/expense-tracker.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
-
-
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-expense-tracker',
@@ -11,19 +10,37 @@ import { ChartsModule } from 'ng2-charts';
 
 
 export class ExpenseTrackerComponent implements OnInit {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+
   public expensesLabels = ['Hotel', 'Transportation', 'Flight', 'Food', 'Attractions', 'Other'];
   public expensesMappings = new Map();
-  
-  public expensesData = [120, 150, 180, 90, 50, 40];
+  public amountToAdd;
+  public amountCategory;
+
+  public hotels = 100;
+  public transportation = 100;
+  public flight = 100;
+  public food = 100;
+  public attractions = 100;
+  public other = 100;
+
+
+
+  public expensesData = [this.hotels, this.transportation, this.flight, this.food, this.attractions, this.other];
   public expensesType = 'pie';
   constructor() { }
 
   ngOnInit() {
 
   }
-
-  updateExpenses() {
-    this.expensesData
+  public barChartOptions = {
+    responsive: true
+  };
+  updateChart(){
+    if (this.amountCategory == undefined) alert("Please select a category.");
+    if (this.amountToAdd == undefined) alert ("Please enter an amount.");
+    this.expensesData[this.amountCategory] += this.amountToAdd;
+    this.chart.chart.update();
   }
 
 }
